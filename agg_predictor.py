f7004be1277795c485d54b3b13b150b203716f84
JACOBIN-SCTCS <jacobja3@gmail.com>
2020-02-05 11:04:43 +0530

    Completed Aggregation Prediction model

--- agg_predictor.py
+++ agg_predictor.py
@@ -4,4 +4,39 @@ import torch.nn.functional as F
 import numpy as np  
 
 class AggPredictor(nn.Module):
-    pass
+
+    def __init__(self, embed_dim,hidden_dim,n_layers=1,dropout=0.2,bidirectional=False):
+
+        super().__init__()
+        
+        self.rnn = nn.LSTM(embed_dim,hidden_dim,n_layers,batch_first=True,dropout=dropout,bidirectional=bidirectional)
+
+        self.attn = nn.Linear(hidden_dim,1)
+       
+        self.softmax = nn.Softmax(dim=1)
+
+        self.agg_out = nn.Sequential(  
+                        nn.Linear(hidden_dim,hidden_dim),
+                        nn.Tanh(),
+                        nn.Linear(hidden_dim,6),
+                )
+
+
+
+    def forward(self,q,hidden):
+
+        output,hidden = self.rnn(q,hidden)
+        att_val = self.attn(output).squeeze()
+
+        att = F.softmax(att_val,dim=1)
+        k_agg = (output * att.unsqueeze(2).expand_as(output)).sum(1)
+        agg_score = self.agg_out(k_agg)
+        return agg_score
+
+'''
+agg_predictor = AggPredictor(50,100)
+q = torch.randn(64,15,50)
+hidden = (torch.randn(1,64,100), torch.randn(1,64,100))
+score = agg_predictor(q,hidden)
+print(score)
+'''
--- test.py
+++ test.py
@@ -2,20 +2,58 @@ from utils import SQLDataset,collate_fn
 from wordembedding import WordEmbedding
 from extract_vocab import load_word_emb
 from torch.utils.data import Dataset,DataLoader
+from agg_predictor import AggPredictor
+import torch.nn as nn
+import torch.optim as optim
+import torch
+import numpy as np
 
 
 
 N_word = 50 
+batch_size =10
+hidden_dim = 100
 
 word_embed = load_word_emb('glove/glove.6B.50d.txt')
 
 sq = SQLDataset('train')
-sql_dataloader = DataLoader(sq,batch_size=5,num_workers=1,collate_fn=collate_fn)
+sql_dataloader = DataLoader(sq,batch_size=batch_size,shuffle=True,num_workers=1,collate_fn=collate_fn)
 g=next(iter(sql_dataloader))
 
 word_emb = WordEmbedding(N_word,word_embed)
 embeddings,len = word_emb.gen_x_batch(g['question_tokens'],g['column_headers'])
-print(embeddings.shape)
+print(embeddings)
+#sql_query = np.asarray(g['sql_query'])
+#agg_tensor = torch.from_numpy(sql_query)
+#print(agg_tensor.shape)
+
+'''
+agg_model = AggPredictor(N_word,hidden_dim)
+epochs = 10
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(agg_model.parameters(),lr=0.01)
 
 
+agg_model.train()
+for e in range(epochs):
+    hidden = (torch.zeros((1,batch_size,hidden_dim)), torch.zeros((1,batch_size,hidden_dim)) )
 
+    for data in  sql_dataloader:
+        hidden = tuple([each.data for each in hidden])
+        agg_model.zero_grad()
+
+        embeddings ,length = word_emb.gen_x_batch(data['question_tokens'],data['column_headers'])
+        scores = agg_model(embeddings,hidden)
+            
+        agg_tensor = torch.from_numpy( np.asarray(data['sql_query']) )
+
+        loss = criterion(scores,agg_tensor)
+        loss.backward()
+        nn.utils.clip_grad_norm_(agg_model.parameters(),5)
+        optimizer.step()
+
+        print('Loss {} ----- {}'.format(e,loss.item()))
+        
+        
+'''
