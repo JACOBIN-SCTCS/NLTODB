6e7f7a734e27885b797b2912d96ca0b02fd214f4
JACOB JAMES K <jacobja3@gmail.com>
2020-05-05 20:30:56 +0530

    Update download_glove.sh

--- download_glove.sh
+++ download_glove.sh
@@ -9,5 +9,5 @@ if [[ ! -d glove ]]; then
 fi
 
 cd glove
-wget http://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip
+wget http://nlp.stanford.edu/data/glove.42B.300d.zip
+unzip glove.42B.300d.zip
