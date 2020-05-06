e2be9254f914914e71e9251bda8dcf16ea36c000
JACOBIN-SCTCS <jacobja3@gmail.com>
2020-05-06 21:14:40 +0530

    generate_exec_jsonl

--- generate_exec_jsonl.py
+++ generate_exec_jsonl.py
@@ -13,6 +13,53 @@ import torch.optim as optim
 
 
 
+def gen_query_acc_for_file( cond_scores, questions ):
+
+    cond_num_score, cond_col_score, cond_op_score, cond_str_score = [
+                x.data.cpu().numpy()  for x in cond_scores
+            ]
+
+    pred_cond = [] 
+
+    for b in range(len(cond_num_score)):
+
+        b_cond = []
+        cond_num = np.argmax(cond_num_score[b])
+
+        all_toks = ['<BEG>'] + questions[b] + ['<END>']
+        max_idxes = np.argsort(-cond_col_score[b])[:cond_num]
+
+        for i in range(int(cond_num)):
+            cur_cond = [] 
+            cur_cond.append(int(max_idxes[i]))
+            cur_cond.append(int(np.argmax(cond_op_score[b][i])))
+            cur_cond_str_toks = []
+
+            for str_score in cond_str_score[b][i]:
+                str_tok = np.argmax(str_score[:len(all_toks)])
+                str_val = all_toks[str_tok]
+                if str_val == '<END>':
+                    break
+                cur_cond_str_toks.append(str_val)
+
+            # Modif Codes can be changed
+            modif_list = []
+            for j in cur_cond_str_toks:
+                if j not in modif_list and j!='<BEG>':
+                    modif_list.append(j)
+
+            cur_cond_str_toks = modif_list
+            #cur_cond.append(merge_tokens( cur_cond_str_toks, "".join(questions[b])  ))
+            cur_cond.append(' '.join(cur_cond_str_toks))
+            b_cond.append(cur_cond)
+        pred_cond.append(b_cond)
+
+    return pred_cond
+
+
+
+
+
 def get_output_as_file(model,test_loader):
 
 
@@ -32,7 +79,7 @@ def get_output_as_file(model,test_loader):
         
         out_agg = torch.argmax( torch.exp(scores[0]),dim=1)
         out_sel = torch.argmax( torch.exp(scores[1]),dim=1)
-        pred_cond = gen_query_acc( scores[2], data['question_tokens']  )
+        pred_cond = gen_query_acc_for_file( scores[2], data['question_tokens']  )
         
         for b in range(len(out_agg)):
 
